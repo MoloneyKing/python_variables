50350cac38fe91930005027df1a0447e1217b8bd
Michelle King <134791551+MoloneyKing@users.noreply.github.com>
2023-07-30 19:54:57 +0100

    Add files via upload
    Python basics based on my examples:
    
    What each data type represents:
    Strings represent text, like names, sentences, etc. We use "" or '' . Integers represent whole numbers. Lists represent ordered groups of data in [] .
    
    Good naming practices:
    Names should be lowercase , use underscores _ for spaces. Avoid symbols. Give names that indicate the variable's purpose clearly.
    
    Combining strings vs math:
    Use + to concatenate strings: 'ice cream' + ' flavor' . Use +, -, *, / for math: 2 + 3
    
    Assignment vs concatenation:
    = is for assigning a value: x = 5
    
    is for concatenating strings: x = 'ice' + ' cream'
    Commas vs + for multiple variables:
    Use + to concatenate strings and assign : x = 'choco' + 'late'.
    Use , to assign multiple variables at once: x,y,z ='choco', 'vanilla','strawberry'
    
    Printing:
    Use print() to see a variable's value or type() to see its data type.
    
    Syntax:
    Be careful with capitalization, whitespace, indentation. Consistency helps readability.
    
    Examples:
    names = ['John', 'Sarah'] #list of strings
    price = 5 #integer
    item = 'Milk' #string
    total = price *2 #math operation
    
    Make concepts concrete with real-life examples like customer data, counts, etc.
    Hope this breakdown of how I would cover these Python basics from your examples is helpful! Let me know if you have any other questions.
    
    Here are detailed step-by-step instructions for working through the Python code examples you provided:
    
    Declare integer variables:
    python
    Copy
    x = 22
    y
    This assigns 22 to x, an integer. y is undefined.
    
    Declare string variables:
    python
    Copy
    y = 'Minct chocolate chip'
    type(y)  
    y is now a string. Use type() to check the data type.
    
    Combine strings with +:
    python
    Copy
    Y = 'Mint Chocolate Chip'
    print(Y)
    Assign multiple variables:
    python
    Copy
    x,y,z = 'Chocolate', 'Vanilla', 'Rocky Road'
    Use lists and indices:
    python
    Copy
    ice_cream = ['Chocolate', 'Vanilla', 'Rocky Road']  
    x,y,z = ice_cream   
    Access list elements with indices in [] .
    
    Use different variable naming styles:
    snake_case
    camelCase
    PascalCase
    Avoid testvar
    Concatenate strings:
    python
    Copy
    x = 'ice Cream is my favourite'  
    y =  ' ,'  
    z = 'yup!'
    print(x + y + z)
    Perform math with variables:
    python
    Copy
    x = 1
    y = 2 
    print(x + y)
    Add, subtract, multiply and divide integers.
    
    Follow these steps to work through and understand the Python syntax, variables, data types and operations in your code examples.

---
+++ python variables - intro lesson.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print ('hello workd')
+
+
+# # First Notebook
+
+# In[2]:
+
+
+x=22
+print(x)
+
+
+# In[3]:
+
+
+type(x)
+
+
+# In[4]:
+
+
+y = 'Minct chocolate chip'
+print (y)
+
+
+# In[5]:
+
+
+type(y)
+
+
+# In[6]:
+
+
+print(y)
+
+
+# In[11]:
+
+
+y = 'Chocolate'
+Y = 'Mint Chocolate Chip'
+print(Y)
+
+
+# In[12]:
+
+
+x,y,z = 'Chocolate', 'Vanilla', 'Rocky Road'
+print(x)
+print(y)
+print(z)
+
+
+# In[14]:
+
+
+x = y = z = 'Root beer Float'
+print(x)
+print(y)
+print(z)
+
+
+# In[17]:
+
+
+ice_cream = ['Chocolate', 'Vanilla', 'Rocky Road']
+x,y,z = ice_cream
+print(x)
+print(y)
+print(z)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+# Camel Case
+#Test Variable Case
+
+testVariableCase = 'Vanialla Swirl'
+
+
+# In[ ]:
+
+
+# Pascal Case
+#Test Variable Case
+
+TestVariableCase = 'Vanialla Swirl'
+
+
+# In[ ]:
+
+
+# Snake Case
+#Test Variable Case
+
+test_variable_case = 'Vanialla Swirl'
+
+
+# In[ ]:
+
+
+testvar = 'Vanialla Swirl'
+test_var = 'Vanialla Swirl'
+_test_var = 'Vanialla Swirl'
+test_variable_case = 'Vanialla Swirl'
+testVar2 = 'Vanialla Swirl'
+
+
+# In[ ]:
+
+
+
+
+
+# In[21]:
+
+
+x = 'ice Cream is my favourite' + ', ' + 'yup!'
+print (x)
+
+
+# In[30]:
+
+
+x = 'ice Cream '
+y = 'is my favourite,'
+z =  ' ' + 'yup!'
+
+print(x+y+z)
+
+
+# In[25]:
+
+
+x = 1
+y = 2
+z = 3
+
+print(x+y+z)
+
+
+# In[31]:
+
+
+x = 'Ice Cream'
+y = 2
+
+print (x,y)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
